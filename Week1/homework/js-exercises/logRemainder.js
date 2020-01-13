74e69a91043bd2cf4e9c671baf82f4ac04254a57
Stefanos <sigianas@yahoo.com>
2020-01-13 17:44:23 +0200

    Only code along left

---
+++ Week1/homework/js-exercises/logRemainder.js
@@ -0,0 +1,4 @@
+"use strict";
+//1. The answer is 1. The number 6 can be divided by 3 so the remainder after the division is 1.
+//2. The answer is 1. The number 20 can be divided by 4 so the remainder after the division is 1.
+//3. The answer is 1. The number 12 can be divided by 2 so the remainder after the division is 1.
---
+++ Week1/homework/js-exercises/logString.js
@@ -0,0 +1,7 @@
+'use strict'
+let myString = "Stefanos Leventis";
+console.log("My full name");
+console.log(myString);
+myString = "The Arkitekt";
+console.log("My nickname as a music producer");
+console.log(myString);
